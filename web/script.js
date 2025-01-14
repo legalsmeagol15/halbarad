2c20a7966269b4c477877c6562abdc6d7b358eed
Wesley Oates <wesley.oates@gmail.com>
2025-01-13 17:08:13 -0700

    Registration form

--- web/script.js
+++ web/script.js
@@ -6,6 +6,11 @@ const closeAboutModal = document.getElementsByClassName('close')[0];
 const loginModal = document.getElementById('loginModal');
 const closeLoginModal = document.getElementsByClassName('close')[1]; // Close button for Login modal
 const mainPage = document.getElementById('mainPage');
+const switchToRegisterButton = document.getElementById('switchToRegister');
+const switchToLoginButton = document.getElementById('switchToLogin');
+const loginForm = document.getElementById('loginForm');
+const registerForm = document.getElementById('registerForm');
+const modalTitle = document.getElementById('modalTitle');
 
 // Open the modal when the About button is clicked
 aboutButton.addEventListener('click', () => {
@@ -34,4 +39,49 @@ function showLoginModal() {
 document.addEventListener('DOMContentLoaded', (event) => {
   // Show the login modal when the document is fully loaded
   showLoginModal();
+});
+
+// Switch to Register Form
+switchToRegisterButton.addEventListener('click', () => {
+  loginForm.style.display = 'none';
+  registerForm.style.display = 'block';
+  //modalTitle.textContent = 'Register';
+});
+
+// Switch to Login Form
+switchToLoginButton.addEventListener('click', () => {
+  registerForm.style.display = 'none';
+  loginForm.style.display = 'block';
+  //modalTitle.textContent = 'Login';
+});
+
+// Handle Login Form submission
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  console.log("Click")
+  event.preventDefault(); // Prevent the default form submission
+
+  // Get the values from the input fields
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  // Basic validation
+  if (username === '' || password === '') {
+      document.getElementById('message').innerText = 'Please fill in all fields.';
+      return;
+  }
+});
+
+// Handle register Form submission
+document.getElementById('registerForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent the default form submission
+
+  // Get the values from the input fields
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  // Basic validation
+  if (username === '' || password === '') {
+      document.getElementById('message').innerText = 'Please fill in all fields.';
+      return;
+  }
 });
