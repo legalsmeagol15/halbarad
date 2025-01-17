bf617e0127190057ad7837e10c0e7de9aaabdc0c
Wesley Oates <wesley.oates@gmail.com>
2025-01-17 09:34:56 -0700

    More work on ntree

--- server/helpers/ntree.go
+++ server/helpers/ntree.go
@@ -1,11 +1,40 @@
 package helpers
 
-type Ntree interface {
+import "errors"
+
+type NTree[T comparable] interface {
+	Add(item T, region Region) error
+	Remove(item T) error
+	GetIntersections(region Region) chan T
+	GetRegion(item T) Region
+	GetBoundary() Region
+	Cardinality() int
 }
-type ntree struct {
+type nTree[T comparable] struct {
+	bounds      region
+	root        nTreeNode[T]
 	cardinality int
+	contents    map[T]Region
+}
+type nTreeNode[T comparable] struct {
+	quad1, quad2, quad3, quad4 *nTreeNode[T]
 }
 
-func NewNTree(cardinality int) Ntree {
-	return ntree{cardinality: cardinality}
+func (t *nTree[T]) Add(item T, region Region) error {
+	if _, ok := t.contents[item]; ok {
+		return errors.New("duplicate item")
+	}
+}
+func (t *nTree[T]) Cardinality() int { return t.cardinality }
+
+func (t *nTree[T]) Remove(item T) error {
+	if _, ok := t.contents[item]; !ok {
+		return errors.New("item does not exist")
+	}
+	delete(t.contents, item)
+	return nil
+}
+
+func NewnTree[T comparable](cardinality int) nTree[T] {
+	return nTree[T]{cardinality: cardinality}
 }
